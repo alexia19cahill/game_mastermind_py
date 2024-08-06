6744f6df0dc8d1597b0bbefd636e570cdb216ca2
Portian02 <135663498+Portian02@users.noreply.github.com>
2024-08-06 00:29:06 -0600

    retroalimentacion al jugador

--- game.py
+++ game.py
@@ -7,3 +7,21 @@ def codigo_aleatorio():
     return [random.choice(colores) for _ in range(4)]
 
 
+def retroalimentacion(adivinanza, codigo):
+    retroalimentacion = []
+    scodigo = codigo[:]
+    cadivinanza = adivinanza[:]
+
+    for i in range(4):
+        if adivinanza[i] == scodigo[i]:
+            retroalimentacion.append('verde')
+            cadivinanza[i] = scodigo[i] = None
+
+    for i in range(4):
+        if cadivinanza[i] and cadivinanza[i] in scodigo:
+            retroalimentacion.append('naranja')
+            scodigo[scodigo.index(cadivinanza[i])] = None
+
+    return retroalimentacion
+
+
